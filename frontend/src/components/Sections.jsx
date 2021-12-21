deab2797ca5982288f6aecf6a27c7daaa9a68660
cerna234 <cerna2424@gmail.com>
2021-12-21 08:21:11 -0800

    responsive file and sections

--- frontend/src/components/Sections.jsx
+++ frontend/src/components/Sections.jsx
@@ -1,51 +1,70 @@
 import React from 'react'
 import styled from 'styled-components'
+import {mobile} from '../responsive'
 
 const Container = styled.div`
-    height: 30vh;
+
     width:100%;
-    background-color: Red;
-    display:flex;
-   
+    background-color:rgb(24, 23, 23);
+    ${mobile({ height: "100vh" })}   
+
+    height: 70vh;
+    display: flex;
 `;
 
-const Text = styled.div`
-    height:100%;
-    width:50%;
-    background-color:blue;
-    text-align:center;
+const Left = styled.div`
+    width: 60%;
+    height: 100%;
+    background-color: blue;
+    display: flex;
+    flex-direction: column;
+
+
+    ${mobile({flexDirection: "row"})};
+`
+
+const Right = styled.div`
+    width: 40%;
+    height: 100%;
+    background-color: #ff7b00;
+`
+
+const Title = styled.div`
+    width: 100%;
+    height: 50%;
+    background-color: green;
+    ${mobile({width: "50%", height: "100%"})};
 
 `
 
-const ImageSection = styled.div`
-    height:100%;
-    width:50%;
-    background-color:purple;
-
-    img{
-        width:200px;
-        height:200px
-        
-    }
+const Subtext = styled.div`
+width: 100%;
+height: 50%;
+background-color: green;
+${mobile({width: "50%", height: "100%"})};
 
 `
+
 
 
 const Sections = (props) => {
 
-    let url = `${props.url}`;
-    
+    var url = props.url
+    var png = props.png
     return (
-        <Container style={{flexDirection: `${props.label}`}} >
-           <Text>
-               <h2>{props.title}</h2>
-               <p>{props.subtext}</p>
-           </Text>
+        <Container>
+            <Left>
+                <Title>
+                    <h2>Title</h2>
+                </Title>
 
-           <ImageSection>
-               <img src={url}></img>
-           </ImageSection>
-            
+                <Subtext>
+                    subtext
+                </Subtext>
+            </Left>
+            <Right>
+                image
+            </Right>
         </Container>
     )
 }
